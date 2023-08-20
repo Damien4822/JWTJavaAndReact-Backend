47fec45d817254e16a5902b7b9e97dffb4602e11
intern.lhdang-batch41 <danglevt1234@gmail.com>
2023-08-21 00:38:27 +0700

    This is Backend

---
+++ src/main/java/auth/auth/config/JwtAuthenticationFilter.java
@@ -0,0 +1,56 @@
+package auth.auth.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final UserDetailsService userDetailsService;
+    private final JwtService service;
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String username;
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+        username = service.extractUsername(jwt);
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+            userDetails.getAuthorities();
+            if (service.isTokenValid(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities());
+                authToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request));
+                //System.out.println(authToken.getAuthorities().toString());
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
---
+++ src/main/java/auth/auth/controller/UserController.java
@@ -0,0 +1,53 @@
+package auth.auth.controller;
+
+import auth.auth.model.User;
+import auth.auth.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("/users")
+@RestController
+public class UserController {
+    public final UserRepository repo;
+
+    @GetMapping("/index")
+    public ResponseEntity<List<User>> getAll() {
+        return ResponseEntity.ok(repo.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getOne(@PathVariable int id) {
+        User user = repo.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("user not found"));
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<User> create(@RequestBody User user) {
+        repo.save(user);
+        return ResponseEntity.ok(user);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> update(@PathVariable int id, @RequestBody User user) {
+        User update = repo.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("user not found"));
+        update.setUsername(user.getUsername());
+        update.setPassword(user.getPassword());
+        repo.save(update);
+        return ResponseEntity.ok(update);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable int id) {
+        User delete = repo.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("user not found"));
+        repo.delete(delete);
+        return ResponseEntity.noContent().build();
+    }
+}
